4e8c9c5d9625e4ed32734e35ef63b8f1896184e6
HomKey <364932863@qq.com>
2020-05-13 21:00:55 +0800

    增加P50 x^n计算 和P102按层级遍历树节点

---
+++ src/leetcode/editor/cn/P102BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,78 @@
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例： 
+//二叉树：[3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+// 
+// Related Topics 树 广度优先搜索
+
+
+package leetcode.editor.cn;
+
+import leetcode.editor.cn.bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//Java：二叉树的层序遍历
+public class P102BinaryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new P102BinaryTreeLevelOrderTraversal().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                List<Integer> list = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode item = queue.poll();
+                    if (item != null) {
+                        list.add(item.val);
+                        if (item.left != null) queue.add(item.left);
+                        if (item.right != null) queue.add(item.right);
+                    }
+                }
+                if (list.size() > 0 ){
+                    result.add(list);
+                }
+            }
+            return result;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
