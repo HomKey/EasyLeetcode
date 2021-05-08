4dba6cec355624c91db7b268de133eddc5c57d46
homkey <552179252@qq.com>
2021-05-08 09:58:47 +0800

    最长回文串

--- src/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ src/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -16,18 +16,70 @@
 
 
 package leetcode.editor.cn;
+
 //Java：最长回文子串
-public class P5LongestPalindromicSubstring{
+public class P5LongestPalindromicSubstring {
     public static void main(String[] args) {
         Solution solution = new P5LongestPalindromicSubstring().new Solution();
         // TO TEST
+        String babad = solution.longestPalindrome("babad");
+        System.out.println(babad);
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String longestPalindrome(String s) {
-        return "";
-    }
-}
-//leetcode submit region end(Prohibit modification and deletion)
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            String res = "";
+            for (int i = 0; i < s.length(); i++) {
+                String s1 = palindrome(s.toCharArray(), i, i);
+                String s2 = palindrome(s.toCharArray(), i, i + 1);
+                res = res.length() > s1.length() ? res : s1;
+                res = res.length() > s2.length() ? res : s2;
+            }
+            return res;
+        }
+
+        private String palindrome(char[] arr, int l, int r) {
+            boolean isPalindrome = false;
+            while (l >= 0 && r < arr.length && arr[l] == arr[r]) {
+                isPalindrome = true;
+                l--;
+                r++;
+            }
+            if (!isPalindrome) return "";
+            l++;
+            r--;
+            char[] res = new char[r - l + 1];
+            System.arraycopy(arr, l, res, 0, r - l + 1);
+            return String.valueOf(res);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
 }
+
+//class Solution {
+//    public String longestPalindrome(String s) {
+//        if (s == null || s.length() < 1) {
+//            return "";
+//        }
+//        int start = 0, end = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            int len1 = expandAroundCenter(s, i, i);
+//            int len2 = expandAroundCenter(s, i, i + 1);
+//            int len = Math.max(len1, len2);
+//            if (len > end - start) {
+//                start = i - (len - 1) / 2;
+//                end = i + len / 2;
+//            }
+//        }
+//        return s.substring(start, end + 1);
+//    }
+//
+//    public int expandAroundCenter(String s, int left, int right) {
+//        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+//            --left;
+//            ++right;
+//        }
+//        return right - left - 1;
+//    }
+//}
