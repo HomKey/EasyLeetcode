0a36431a2e1a564b9a8e0ae5e1f6b05ad138ef8e
homkey <552179252@qq.com>
2021-05-26 15:49:08 +0800

    滑动窗口最大值

--- src/leetcode/editor/cn/P239SlidingWindowMaximum.java
+++ src/leetcode/editor/cn/P239SlidingWindowMaximum.java
@@ -43,20 +43,62 @@ package leetcode.editor.cn;
 import leetcode.editor.cn.bean.ListNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 //Java：滑动窗口最大值
-public class P239SlidingWindowMaximum{
+public class P239SlidingWindowMaximum {
     public static void main(String[] args) {
         Solution solution = new P239SlidingWindowMaximum().new Solution();
         // TO TEST
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        return null;
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            MonotonicQueue window = new MonotonicQueue();
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (i < k - 1) {
+                    //先填满窗口的前 k - 1
+                    window.push(nums[i]);
+                } else {
+                    // 窗口向前滑动，加入新数字
+                    window.push(nums[i]);
+                    // 记录当前窗口的最大值
+                    res.add(window.max());
+                    // 移出旧数字
+                    window.pop(nums[i - k + 1]);
+                }
+            }
+            int[] arr = new int[res.size()];
+            for (int i = 0; i < res.size(); i++) {
+                arr[i] = res.get(i);
+            }
+            return arr;
+        }
+
+
+        private class MonotonicQueue {
+            LinkedList<Integer> linkedList = new LinkedList<>();
+
+            public void push(int n){
+                while (!linkedList.isEmpty() && n > linkedList.getLast()){
+                    linkedList.pollLast();
+                }
+                linkedList.addLast(n);
+            }
+
+            public Integer max(){
+                return linkedList.getFirst();
+            }
+
+            public void pop(int n){
+                if (n == linkedList.getFirst()) linkedList.pollFirst();
+            }
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
